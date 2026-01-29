e6be345011f768fae6b7b79cbca7ba231f52567a
Elena Ciolac <nordia.vanzari@gmail.com>
2026-01-29 23:45:36 +0000

    it failed to publish

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,9 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  devIndicators: {
-    allowedDevOrigins: [
-      'https://6000-firebase-studio-1769699917318.cluster-2a24trvdezeggvmpy7fccga2ee.cloudworkstations.dev',
-    ],
-  },
+  allowedDevOrigins: [
+    'https://6000-firebase-studio-1769699917318.cluster-2a24trvdezeggvmpy7fccga2ee.cloudworkstations.dev',
+  ],
   typescript: {
     ignoreBuildErrors: true,
   },
