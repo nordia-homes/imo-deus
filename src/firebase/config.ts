6d51337a015489fb5fd0fb1bd926f97422371aca
Elena Ciolac <nordia.vanzari@gmail.com>
2026-01-29 18:40:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-652232171-42fb6",
+  "appId": "1:552699648501:web:971d6d94f88bd519f4a8bc",
+  "apiKey": "AIzaSyBe2Kd5E2yz6SvRurD0zdeLcORGInCLFoY",
+  "authDomain": "studio-652232171-42fb6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "552699648501"
+};
