e9302df5f523517f40b9cd751900c272c5a01340
Elena Ciolac <nordia.vanzari@gmail.com>
2026-01-30 07:50:10 +0000

    Try fixing this error: `Unhandled Runtime Error: ReferenceError: CardFoo

--- src/app/(dashboard)/settings/page.tsx
+++ src/app/(dashboard)/settings/page.tsx
@@ -9,7 +9,7 @@ import { collection, doc, writeBatch } from 'firebase/firestore';
 import type { UserProfile, Agency } from '@/lib/types';
 import { useToast } from '@/hooks/use-toast';
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
