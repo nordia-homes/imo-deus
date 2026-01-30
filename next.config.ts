8fdad11597d8841fbc71e276d01cf7f4c86c8ac2
Elena Ciolac <nordia.vanzari@gmail.com>
2026-01-30 00:15:09 +0000

    it failed to publish. Maybe because of this? /bin/sh -c trap "[ -f /work

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    allowedDevOrigins: [
-      'https://6000-firebase-studio-1769699917318.cluster-2a24trvdezeggvmpy7fccga2ee.cloudworkstations.dev',
-    ],
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
