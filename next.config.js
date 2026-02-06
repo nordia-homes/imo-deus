5aec0bdd15b609b4bf8fa230e17be2e77aafc475
Elena Ciolac <nordia.vanzari@gmail.com>
2026-02-06 09:21:26 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Invalid src prop

--- next.config.js
+++ next.config.js
@@ -39,6 +39,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'render.openstreetmap.org',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   experimental: {
