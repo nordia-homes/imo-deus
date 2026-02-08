855c820d65b4ebf8ad7488e702c5cd63318496fb
Elena Ciolac <nordia.vanzari@gmail.com>
2026-02-08 11:56:49 +0000

    bara laterala a meniului principal al aplicatiei se deshide intr-o culoa

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -204,7 +204,7 @@ const Sidebar = React.forwardRef<
           <SheetContent
             data-sidebar="sidebar"
             data-mobile="true"
-            className="w-[--sidebar-width] bg-sidebar p-0 text-sidebar-foreground [&>button]:hidden"
+            className="w-[--sidebar-width] bg-[#f8f8f9] p-0 text-black [&>button]:hidden"
             style={
               {
                 "--sidebar-width": SIDEBAR_WIDTH_MOBILE,
