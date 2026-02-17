252cf46d86301e3f3acfc423237ea2004cdc133f
Elena Ciolac <nordia.vanzari@gmail.com>
2026-02-17 16:44:40 +0000

    pe tableta, s-a stricat pagina stanga-dreapta.

--- src/app/(dashboard)/leads/[leadId]/page.tsx
+++ src/app/(dashboard)/leads/[leadId]/page.tsx
@@ -578,7 +578,7 @@ export default function LeadDetailPage() {
     }
 
     return (
-        <div className="h-full flex flex-col md:-mx-6 lg:mx-0">
+        <div className="h-full flex flex-col">
              {/* Mobile View: Dark, app-like */}
             <div className='lg:hidden bg-[#0F1E33] -mt-6 pb-4'>
                 <div className="pt-4 space-y-4">
@@ -717,7 +717,7 @@ export default function LeadDetailPage() {
             </div>
             
             {/* Desktop View: Grid */}
-            <div className="hidden lg:block h-full bg-[#0F1E33] -mt-6 -mx-8 -mb-6 px-8 pt-6 pb-6 text-white">
+            <div className="hidden lg:block h-full bg-[#0F1E33] -mt-6 -mx-6 -mb-6 px-6 pt-6 pb-6 text-white">
                  <main className="grid lg:grid-cols-12 gap-6 items-start">
                     <div className="lg:col-span-4 space-y-6">
                         <Card className="bg-[#152A47] text-white border-none rounded-2xl p-4 space-y-4">
