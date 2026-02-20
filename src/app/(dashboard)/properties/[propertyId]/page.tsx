d2ef172e7c45c7d1bcb0d7b3b439a5b2bd25063f
Elena Ciolac <nordia.vanzari@gmail.com>
2026-02-20 12:15:56 +0000

    pe desktop, pagina detalii proprietate vreau sa aiba margini stanga-drea

--- src/app/(dashboard)/properties/[propertyId]/page.tsx
+++ src/app/(dashboard)/properties/[propertyId]/page.tsx
@@ -360,7 +360,7 @@ export default function PropertyDetailPage() {
 
     return (
         <div className="h-full">
-            <div className="hidden lg:block h-full bg-[#0F1E33] -mt-6 -mx-6 -mb-6 px-6 pt-6 pb-6 text-white">
+            <div className="hidden lg:block h-full bg-[#0F1E33] -mt-6 -mb-6 px-3 pt-6 pb-6 text-white">
                 <PropertyHeader 
                     property={property} 
                     onTriggerAddViewing={() => setIsAddViewingOpen(true)}
